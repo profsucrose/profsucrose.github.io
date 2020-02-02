502cff561ccca8731d533a40d28f9de2e5babee2
Prof. Sucrose <dr.blobbles@gmail.com>
2020-02-02 01:45:01 -0800

    Added animations

--- main.js
+++ main.js
@@ -4,23 +4,27 @@ fetch('https://cors-anywhere.herokuapp.com/https://github.com/profsucrose?tab=re
     .then(html => {
         const rawRepos = html.match(/Code">[\s\S]*?<\/li/g).map(x => x.match(/>[\s\S]*?</g))
         const repos = rawRepos.map(x => x.map(y => y.split('').filter(c => c !== '<' && c !== '>' && c !== '\n').join('').trim()).filter(x => x))
-        
-        repos.forEach(repo => {
+
+        repos.forEach((repo, index) => {
             console.log(repo)
             const title = repo[0]
             const link = `https://github.com/profsucrose/${title}`
-            const description = !repo[1].toLowerCase().includes('fork')
-                                    ? repo[1].split(' ').length > 1 
-                                        ? repo[1] 
-                                        : '[No description]'
-                                    : 'Forked repo'
+            const date = repo[repo.length - 1]
+            const language = repo[2] === 'Updated' ? '' : repo[2]
+            const description = !(title === 'profsucrose.github.io')
+                                    ? !repo[1].toLowerCase().includes('fork')
+                                            ? repo[1].split(' ').length > 1 
+                                                ? repo[1] 
+                                                : '[No description]'
+                                            : 'Forked repo'
+                                    : 'The source for THIS very website!'
             document.querySelector('.cards').innerHTML += `
-                <div class="box" onClick="() => {alert('click'); location.href='${link}'}">
+                <div class="box" onClick="() => {alert('click'); location.href='${link}'}" style="animation-delay:${index / 20}s">
                     <div class="media-content">
-                        <p>
-                            <strong><a href="${link}">${title}</a></strong>
-                            <br>${description}</br>
-                        </p>
+                        <strong><a href="${link}">${title}</a></strong>
+                        <p class="description">${description}</p>
+                        <p class="language">${language}</p>
+                        <footer>${date}</footer>
                     </div>
                 </div>
             `
